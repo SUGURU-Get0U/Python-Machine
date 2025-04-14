77bdecb0967d0e6eccbc4cdb0adf558a93d5c23f
Liam <twich.euedu@gmail.com>
2025-04-14 09:35:21 -0300

    starting chess project

---
+++ automaçã.py
@@ -0,0 +1,23 @@
+
+# Creating a code that opens chess.com, queues for a match and plays the best moves
+# according to the chess database
+
+# open google
+import pyautogui
+
+pyautogui.press("win") # opens the win console
+pyautogui.write("chrome")
+pyautogui.press("enter")
+
+# Open chess.com
+pyautogui.write("https://www.chess.com/")
+
+# Log in without an account
+# queue for a match
+# if playing with black wait for  move input
+# check the database for possible moves
+# check the coordinates of this move
+# plays the move
+
+
+
